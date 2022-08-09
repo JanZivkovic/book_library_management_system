eb481b1720ab2e4e772541e8408558b3c0aba1e3
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-09 09:52:01 +0200

    Refactored BookLoan validations

--- app/models/book_loan.rb
+++ app/models/book_loan.rb
@@ -2,8 +2,10 @@ class BookLoan < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates :start_date, presence: true, comparison: { greater_than_or_equal: :end_date }
-  validates :end_date, presence: true, comparison: { less_than_or_equal: :end_date }
+  validates :start_date, presence: true
+  validates :start_date, comparison: { greater_than_or_equal_to: :end_date }
+  validates :end_date, presence: true
+  validates :end_date, comparison: { less_than_or_equal_to: :start_date }
   validates :book, presence: true
   validates :user, presence: true
 end
