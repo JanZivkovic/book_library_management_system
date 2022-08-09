3829d40560fe6a046e6155d7a169758796df4ee8
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-09 22:56:55 +0200

    Added AuthorsController and created tests for it

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,65 @@
+class AuthorsController < ApplicationController
+
+  before_action :authenticate_request, except: %i[ index show search]
+  before_action :check_user_permissions, only: %i[ create update destroy ]
+  before_action :set_author, only: %i[ show update destroy ]
+
+  # GET /authors
+  def index
+    @authors = Author.all
+
+    render json: @authors
+  end
+
+  # GET /authors/1
+  def show
+    render json: @author
+  end
+
+  # POST /authors
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      render json: @author, status: :created, location: @author
+    else
+      render json: @author.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /authors/1
+  def update
+    if @author.update(author_params)
+      render json: @author
+    else
+      render json: @author.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /authors/1
+  def destroy
+    begin
+      @author.destroy
+    rescue ActiveRecord::InvalidForeignKey => e
+      render json: { error: 'Foreign key constraint violated.' }, status: :conflict
+    end
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def author_params
+      params.require(:author).permit(:name)
+    end
+
+    def check_user_permissions
+      if @current_user.role.name != 'LIBRARIAN'
+        render json: { error: 'forbidden' }, status: :forbidden
+      end
+    end
+end
---
+++ test/controllers/authors_controller_test.rb
@@ -0,0 +1,102 @@
+require "test_helper"
+
+class AuthorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @author = authors(:terry_pratchett)
+    @author_with_no_books = authors(:author_with_no_books)
+    #TODO: should be extracted into helper
+    post auth_login_path, {params: {username: 'ppetrovic', password: 'harD2gue55'}}
+    @token_member = JSON.parse(response.body)['token']
+
+    post auth_login_path, {params: {username: 'pstijena', password: 'riirgjt45893'}}
+    @token_librarian = JSON.parse(response.body)['token']
+
+  end
+
+  test "should get index" do
+    get authors_url, headers:{authorization: @token_member}, as: :json
+    assert_response :success
+  end
+
+  test "should create author" do
+    assert_difference("Author.count") do
+      post authors_url,
+           headers:{authorization: @token_librarian},
+           params: { author: {name: 'Don Huan Dela Puerta'  } },
+           as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should not create author and should receive 403 Forbidden" do
+    assert_no_difference("Author.count") do
+      post authors_url,
+           headers:{authorization: @token_member},
+           params: { author: {name: 'Don Huan Dela Puerta'  } },
+           as: :json
+    end
+
+    assert_response :forbidden
+  end
+
+  test "should show author" do
+    get author_url(@author), headers:{authorization: @token_member},as: :json
+    assert_response :success
+  end
+
+  test "should update author" do
+    assert_changes '@author.name' do
+      patch author_url(@author),
+           headers:{authorization: @token_librarian},
+           params: {author: {name: "#{@author.name} Junior"  } },
+           as: :json
+      @author.reload
+    end
+
+    assert_response :success
+  end
+
+  test "should not update author and should get 403 Forbidden" do
+    assert_no_changes '@author.name' do
+      patch author_url(@author),
+            headers:{authorization: @token_member},
+            params: {author: {name: "#{@author.name} Junior"  } },
+            as: :json
+      @author.reload
+    end
+
+    assert_response :forbidden
+  end
+
+  test "should destroy author" do
+    assert_difference("Author.count", -1, 'User with librarian role should be able to delete an author') do
+      delete author_url(@author_with_no_books),
+             headers:{authorization: @token_librarian},
+             as: :json
+    end
+
+    assert_response :no_content
+  end
+
+  test "should not destroy author with books should get 409 conflict" do
+    assert_no_difference('Author.count', 'Author that has associated books can\'t be destroyed') do
+      delete author_url(@author),
+             headers:{authorization: @token_librarian},
+             as: :json
+    end
+
+    assert_response :conflict
+  end
+
+  test "should not destroy author and should get 403 Forbidden" do
+    assert_no_difference("Author.count",
+                         'User with member role should not be permitted to delete an author') do
+      delete author_url(@author_with_no_books),
+             headers:{authorization: @token_member},
+             as: :json
+    end
+
+    assert_response :forbidden
+  end
+end
