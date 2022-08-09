a2541aaf21058483ae433a5b3321663b151f83c1
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-09 12:08:50 +0200

    Added AuthenticationController and its route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users
+  resources :users , only: [:create]
+  post 'auth/login', to: 'authentication#login'
+
 end
