fc111ea1ab3e7f35fff3fbbb4d6bb105a94b0063
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-09 10:09:45 +0200

    Refactored json_web_token.rb and removed secret key base from dev env

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -1,13 +1,10 @@
-backend_authentication > app > controllers > concerns > json_web_token.rb
-# frozen_string_literal: true
 
 require "jwt"
 module JsonWebToken
   extend ActiveSupport::Concern
   SECRET_KEY = Rails.application.secret_key_base
-  JWT_EXPIRES_IN = Rails.application.jwt_expires_in_days
 
-  def jwt_encode(payload, exp = JWT_EXPIRES_IN.days.from_now)
+  def jwt_encode(payload, exp = 7.days.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
