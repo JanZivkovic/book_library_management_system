273884d398dcfbf2f6e7539eb89edf6422d42e1a
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-09 12:36:10 +0200

    Added basic tests for AuthenticationController
    Added basic login tests
    Added password digest data to fixtures

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class AuthenticationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "login_with_valid_user_receive_success_and_jwt" do
+    post '/auth/login',
+         params: {username: 'ppetrovic', password: 'harD2gue55'}
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_not_nil json_response['token'], 'JWT token is nil'
+  end
+
+  test "login_with_invalid_user_receive_unauthorized" do
+    post '/auth/login',
+         params: {username: 'IdoNotExist', password: 'harD2gue55'}
+    assert_response :unauthorized
+    json_response = JSON.parse(response.body)
+    assert_equal json_response['error'], 'unauthorized', 'Received unexpected error message'
+  end
 end
