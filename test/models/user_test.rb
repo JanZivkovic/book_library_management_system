c7c5dc2530996cbd8bf006f27f6be497d3f9daad
JanZivkovic <JanZivkovic@users.noreply.github.com>
2022-08-10 21:05:10 +0200

    Fix: wrong case

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,12 +4,12 @@ class UserTest < ActiveSupport::TestCase
 
   test "can make a loan should return true" do
     user = users(:three)
-    assert user.canMakeALoan? Date.today
+    assert user.can_make_a_loan? Date.today
   end
 
   test "can make a loan should return false" do
     user = users(:one)
-    assert_not user.canMakeALoan? Date.today
+    assert_not user.can_make_a_loan? Date.today
   end
 
 end
